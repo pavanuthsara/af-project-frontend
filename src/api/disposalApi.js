ca87c2ba2b2da6952da03f55fdb04249834e3018
Pavan Uthsara <uthsarapavan@gmail.com>
2026-04-12 08:29:00 +0530

    fix disposal stat route issue

--- src/api/disposalApi.js
+++ src/api/disposalApi.js
@@ -5,5 +5,5 @@ export const getDisposalHistory = (params) => api.get('/disposal/history', { par
 export const updateDisposal = (id, data) => api.put(`/disposal/${id}`, data);
 export const deleteDisposal = (id) => api.delete(`/disposal/${id}`);
 export const getDisposalStats = () => api.get('/disposal/stats');
-export const getWasteStats = () => api.get('/disposal/waste-stats');
+export const getWasteStats = () => api.get('/disposal/stats');
 
